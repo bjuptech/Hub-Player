80825286abde4330731ec7981d85059d5b23e195
tshaf725 <55199359+tshaf725@users.noreply.github.com>
2019-09-11 13:04:16 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,21 @@
+videojs.registerPlugin('customEvent', function() {
+    var myPlayer = this,
+		document.onkeydown = function(event) {
+      switch (event.keyCode) {
+         case 37:
+              event.preventDefault();
+              
+              vid_currentTime = myPlayer.currentTime;
+              myPlayer.currentTime = vid_currentTime - 5;
+            break;
+         
+         case 39:
+              event.preventDefault();
+              
+              vid_currentTime = myPlayer.currentTime;
+              myPlayer.currentTime = vid_currentTime + 5;
+            break;
+
+      myPlayer.currentTime(newTime);
+    });
+});
