75b425faedd84f8ac801ccecaa14bebd18682f2e
tshaf725 <55199359+tshaf725@users.noreply.github.com>
2019-09-11 11:13:05 -0400

    Update numkeys.js

--- numkeys.js
+++ numkeys.js
@@ -1,21 +1,19 @@
-videojs.registerPlugin('numkeys', function() {
-var myPlayer = this
+var theVideo = document.getElementById("cspd_video");
   document.onkeydown = function(event) {
       switch (event.keyCode) {
          case 37:
               event.preventDefault();
               
-              vid_currentTime = myPlayer.currentTime;
-              myPlayer.currentTime = vid_currentTime - 5;
+              vid_currentTime = theVideo.currentTime;
+              theVideo.currentTime = vid_currentTime - 5;
             break;
          
          case 39:
               event.preventDefault();
               
-              vid_currentTime = myPlayer.currentTime;
-              myPlayer.currentTime = vid_currentTime + 5;
+              vid_currentTime = theVideo.currentTime;
+              theVideo.currentTime = vid_currentTime + 5;
             break;
          
       }
-  };
   };
