23f198713316664a0d3fa9fd09af7b95e6531f26
tshaf725 <55199359+tshaf725@users.noreply.github.com>
2019-09-11 11:20:34 -0400

    Update numkeys.js

--- numkeys.js
+++ numkeys.js
@@ -6,22 +6,37 @@ videojs.registerPlugin('customEvent', function() {
         newElement = document.createElement('div'),
         newImage = document.createElement('img');
 
-        
+    // Assign id and classes to div for icon
+    newElement.id = 'backButton';
 
-    document.onkeydown = function(event) {
-      switch (event.keyCode) {
-         case 37:
-              event.preventDefault();
-              
-              vid_currentTime = theVideo.currentTime;
-              theVideo.currentTime = vid_currentTime - 5;
-            break;
-         
-         case 39:
-              event.preventDefault();
-              
-              vid_currentTime = theVideo.currentTime;
-              theVideo.currentTime = vid_currentTime + 5;
-            break;
+    // Assign properties to elements and assign to parents
+    newImage.setAttribute('src','http://solutions.brightcove.com/bcls/brightcove-player/custom-event/back-button.png');
+    newElement.appendChild(newImage);
+
+    // Get the spacer element
+    spacer = document.getElementsByClassName('vjs-spacer')[0];
+    // Place the new element in the spacer
+    spacer.appendChild(newElement);
+
+    backButton = document.getElementById("backButton");
+
+    backButton.onclick = function() {
+      // trigger a custom event with data
+      myPlayer.trigger('rewind', {'amount': rewindAmount});
+    }
+
+    // listen for a custom event with data
+    myPlayer.on('rewind',function(evt,data){
+      var newTime,
+          rewindAmt = data.amount,
+          videoTime = myPlayer.currentTime();
+
+      if (videoTime >= rewindAmt) {
+        newTime = videoTime - rewindAmt;
+      } else {
+        newTime = 0;
+      }
+
+      myPlayer.currentTime(newTime);
     });
 });
